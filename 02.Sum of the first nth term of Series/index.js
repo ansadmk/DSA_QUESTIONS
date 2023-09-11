077920d96c0a878b7d7f892efc8a783ab645b4e1
ansadmk <ansadmk@gmail.com>
2023-09-11 16:32:35 +0530

    second commit

--- 02.Sum of the first nth term of Series/index.js
+++ 02.Sum of the first nth term of Series/index.js
@@ -1,13 +1,14 @@
 function SeriesSum(n)
 {
-  var str=1.00
+  var str=0
   for(i=0;i<n;i++){
     if(i!=0){
       str+=1/(i*3+1)
+    }else{
+      str=1
     }
    
   }
-  console.log(str.toFixed(2));
   return str.toFixed(2)
   
 }
